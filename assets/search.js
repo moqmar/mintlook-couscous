0d4c1f037a6c2f9bf7b2d2c94f176f06aca9443c
Moritz Marquardt <git@mo-mar.de>
2018-02-27 00:10:44 +0100

    Search current language only

--- assets/search.js
+++ assets/search.js
@@ -4,12 +4,18 @@ function search(self) {
     if (self) search.form = self;
     search.working = true;
     if (!search.engine) {
+        if (document.querySelector(".language-select-popup")) {
+            search.availableLanguages = Array.from(document.querySelectorAll(".language-select-popup img")).map(function(x) { return x.src.match(/\/flags\/([a-z]+)\.png$/)[1] });
+            search.defaultLanguage = document.getElementsByClassName("language-select-popup")[0].getAttribute("data-default-language");
+            search.lang = document.documentElement.lang;
+        }
         fetch(search.form.getAttribute("data-baseurl") + "/search-index.json").then(function(r) { return r.json() }).then(function(data) {
             search.engine = new JsSearch.Search("url");
             search.engine.addIndex("title");
             search.engine.addIndex("description");
             search.engine.addIndex("content");
 
+            search.index = data;
             search.engine.addDocuments(data);
 
             search.working = false;
@@ -25,6 +31,14 @@ function search(self) {
     var output = "";
 
     for (var i = 0; i < result.length; i++) {
+        var lang = result[i].url.replace(/^\/+|\/+$|\/+(?=\/)/g, "").split("/")[0];
+        if (search.lang && lang != search.lang) { // Different language
+            // Search language is default language, but result language is different
+            if (search.lang == search.defaultLanguage && search.availableLanguages.indexOf(lang) > -1) continue;
+            // Different language and not default language
+            if (search.lang != search.defaultLanguage) continue;
+        }
+
         var el = document.createElement("a");
         el.href = search.form.getAttribute("data-baseurl") + result[i].url;
         
